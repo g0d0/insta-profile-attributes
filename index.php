111aa0c729f477aadaec1bf85c47d2b4ce934e43
Antonio M Quadros Filho <antoniomquadrosfilho@gmail.com>
2020-09-26 21:16:52 -0300

    remove commented code

--- index.php
+++ index.php
@@ -5,19 +5,13 @@ define('ATTRS', explode('.', $argv[2]));
 
 $url = 'https://www.instagram.com/web/search/topsearch/?context=user&query=' . USERNAME;
 
-// create curl resource
 $ch = curl_init();
 
-// set url
 curl_setopt($ch, CURLOPT_URL, $url);
-
-//return the transfer as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-// $output contains the output string
 $output = json_decode(curl_exec($ch), true);
 
-// close curl resource to free up system resources
 curl_close($ch);
 
 $output = array_reduce(ATTRS, function ($accumulator, $item) use ($output) {
